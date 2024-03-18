e8a1e1927be346e3831608fa829e31592f76d89b
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-03-18 21:34:39 +0300

    Yaş Tespiti görevi örnek koşul olarak verilen 3 durumda da sağlanıyor.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,13 @@ public class Main {
         boolean isBarking=false;
         int clock=2;
         boolean shouldWake = shouldWakeUp(isBarking,clock);
+        int firstAge = 22;
+        int secondAge = 23;
+        int thirdAge = 34;
+        boolean hasTeenQuestion = hasTeen(firstAge, secondAge, thirdAge);
         System.out.println("Hello world!");
         System.out.println("Should wake up? " + shouldWake);
+        System.out.println("Are they teen?" + hasTeenQuestion);
     }
 
 
@@ -26,7 +31,10 @@ public class Main {
 
 
 public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+    if ((firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19)) {
+        return true;
+    } else {
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
