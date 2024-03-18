7c485b79f9f89e4c44996c1718a201cd1917f559
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-03-18 19:59:12 +0300

    Öğrenci numarası değiştirildi. Görev 1 denemesi yapıldı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        boolean isBarking=true;
+        int clock=1;
+        boolean shouldWake = shouldWakeUp(isBarking,clock);
         System.out.println("Hello world!");
     }
 
+
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (isBarking) {
+            if (clock >= 8 && clock <= 20) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
