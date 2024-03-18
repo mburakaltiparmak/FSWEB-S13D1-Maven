4ed2aac6520232b2e10d93fd8be0bdc140156bad
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-03-18 20:08:20 +0300

    jdoodle isimli online compiler üzerinde ilk fonksiyonu çalıştırdım. shouldWakeUp görevi yapılmıştır.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,30 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        boolean isBarking=true;
-        int clock=1;
+        boolean isBarking=false;
+        int clock=2;
         boolean shouldWake = shouldWakeUp(isBarking,clock);
         System.out.println("Hello world!");
+        System.out.println("Should wake up? " + shouldWake);
     }
 
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
         if (isBarking) {
             if (clock >= 8 && clock <= 20) {
-                return true;
-            } else {
                 return false;
+            } else {
+                return true;
             }
         }
         return false;
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+
+}
+
+
+public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
        
     }
 
